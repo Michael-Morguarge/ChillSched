16ef1d750a757b50a0304f2f2f919cd75488ac1d
Michael Morguarge <mikeyjm14@gmail.com>
2020-05-16 14:36:07 -0400

    Updates to project types

---
+++ FrontendConsole/Interfaces/ICustomConsoleColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Customize.Interface
+{
+    public interface ICustomConsoleColor
+    {
+        void DisplayText(string text);
+        void Actions();
+    }
+}
---
+++ FrontendConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp.Customize.Implementation;
+using ConsoleApp.Customize.Interface;
+using System;
+
+namespace FrontendConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomConsoleColor menu = new MenuFormat();
+
+            menu.DisplayText("Hello World!");
+            menu.Actions();
+
+            Console.WriteLine("\r\nGoodbye");
+        }
+    }
+}
